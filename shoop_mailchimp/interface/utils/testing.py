883c09e5b312bff21a7bfba15bad4c3edab50ea4
Shawn Her Many Horses <shawnhmh@gmail.com>
2016-04-04 15:26:03 -0700

    Fix configuration check errors
    Add checks for invalid server configuration and make sure that
    exceptions from rejected Mailchimp requests are properly displayed.
    
    Refs SHOOP-2303

--- shoop_mailchimp/interface/utils/testing.py
+++ shoop_mailchimp/interface/utils/testing.py
@@ -4,9 +4,20 @@
 #
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
+from requests import ConnectionError
+
 from shoop_mailchimp.interface.base import ShoopMailchimp
 
 
 def interface_test(shop):
     client = ShoopMailchimp(shop)
-    return bool(client.get_list())
+
+    try:
+        results = client.get_list()
+    except ConnectionError:
+        return False
+
+    status = None
+    if results:
+        status = results.get("status", None)
+    return False if (status and status != 200) else bool(results)
